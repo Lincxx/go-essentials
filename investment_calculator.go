1b4994bb1d8739e162d22c8999122a9173bd1c5b
lincxx <bossc@BOSSCO>
2023-12-21 12:01:46 -0500

    Function - return val & variable scope

--- investment_calculator.go
+++ investment_calculator.go
@@ -5,8 +5,10 @@ import (
 	"math"
 )
 
+const inflationRate = 2.5
+
 func main() {
-	const inflationRate = 2.5
+	
 	var investmentAmount float64
 	var years float64
 	expectedReturnRate := 5.5
@@ -24,15 +26,17 @@ func main() {
 	outputText("How many years: ")
 	fmt.Scan(&years)
 
-	futureValue := investmentAmount * math.Pow(1 + expectedReturnRate / 100, years)
-	futureRealValue := futureValue / math.Pow(1 + inflationRate/100, years)
+	futureValue, futureRealValue := calculateFutureValues(investmentAmount, expectedReturnRate, years)
+	
+	//futureValue := investmentAmount * math.Pow(1 + expectedReturnRate / 100, years)
+	//futureRealValue := futureValue / math.Pow(1 + inflationRate/100, years)
 
 
 
-	// formattedFV := fmt.Sprintf("Future Value: %.2f\n", futureValue)
-	// formattedRFV := fmt.Sprintf("Future Value (adjusted for inflation): %.2f\n", futureRealValue)
-	// fmt.Print(formattedFV, formattedRFV)
-	// fmt.Println("Future Value: ", futureValue)
+	formattedFV := fmt.Sprintf("Future Value: %.2f\n", futureValue)
+	formattedRFV := fmt.Sprintf("Future Value (adjusted for inflation): %.2f\n", futureRealValue)
+	fmt.Print(formattedFV, formattedRFV)
+	fmt.Println("Future Value: ", futureValue)
 
 	// the backtick will use the format we have set. So if there are newline or tabs they will be shown
 	// fmt.Printf(`Future Value: %.2f\n 
@@ -43,4 +47,11 @@ func main() {
 
 func outputText(text string) {
 	fmt.Print(text)
+}
+
+func calculateFutureValues(investmentAmount float64 , expectedReturnRate float64,  years float64) (float64, float64) {
+	//we can return multiple with a comma at the end of the first return
+	fv := investmentAmount * math.Pow(1 + expectedReturnRate / 100, years)
+	rfv := fv / math.Pow(1 + inflationRate/100, years)
+	return fv, rfv
 }
