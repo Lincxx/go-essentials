88374092545686d7e75383f3f923af4cc091781d
Lincxx <bossco28@gmail.com>
2023-12-23 15:10:09 -0500

    if and else if

--- go-basics/bank.go
+++ go-basics/bank.go
@@ -5,6 +5,8 @@ import (
 )
 
 func main() {
+    var accountBalance = 1000.0
+
 	fmt.Println("Welcome to Go Bank!!!")
 	fmt.Println("What would you like to do?")
 	fmt.Println("1. Check balance")
@@ -17,6 +19,26 @@ func main() {
 	fmt.Print("Your choice: ")
 	fmt.Scan(&choice)
 
-	//preform action based on the user selection
+	//another way to set a bool
+	//wantsCheckBalance := choice == 1
 
+	//preform action based on the user selection
+	if choice == 1{
+		fmt.Println("Your balance is:", accountBalance)
+	} else if choice == 2 {
+		fmt.Println("Your deposit:")
+		var depositAmount float64
+		fmt.Scan(&depositAmount)
+		accountBalance += depositAmount
+		fmt.Println("New Balance:", accountBalance)
+	} else if choice == 3 {
+		fmt.Println("Withdrawal amount:")
+		var withdrawalAmount float64
+		fmt.Scan(&withdrawalAmount)
+		accountBalance -= withdrawalAmount
+		fmt.Println("New Balance:", accountBalance)
+	} else {
+		fmt.Println("Have a nice day")
+	}
+	
 }
