775aa2255277c65a3d9f1f9e91f6795ce69b5e64
lincxx <bossc@BOSSCO>
2023-12-26 06:53:45 -0500

    Using pointers & passing pointers to function

--- undertanding-pointers/main.go
+++ undertanding-pointers/main.go
@@ -15,7 +15,7 @@ func main() {
 	//agePointer := &age
 
 	//prints out the address - might not be to useful here, maybe when we pass it to a function it will be
-	fmt.Println("Age", agePointer)
+	//fmt.Println("Age", agePointer)
 
 	//if we want the actual value we'll need to do something called dereferencing
 	fmt.Println("Age", *agePointer)
@@ -25,8 +25,16 @@ func main() {
 	//adultYears := getAdultYears(age)
 	//fmt.Println(adultYears)
 
+	editAdultYears(agePointer)
+	fmt.Println(age)
+
 }
 
-func getAdultYears(age int) int {
-	return age - 18
+// the name of the function states we are getting adult years (getAdultYears) and not editing them. Change the name
+// to editAdultYears
+func editAdultYears(age *int) {
+	//return *age - 18
+	//deference - then we are overriding that value in age
+	*age = *age - 18
+	//this could be a problem or unexpected behavior, that we wrote over a variable.
 }
