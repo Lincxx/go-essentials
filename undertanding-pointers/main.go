e564abf94547a4fac25f9c75d6c630c3d04034dd
lincxx <bossc@BOSSCO>
2023-12-26 06:41:27 -0500

    Pointer as values

--- undertanding-pointers/main.go
+++ undertanding-pointers/main.go
@@ -7,19 +7,23 @@ func main() {
 	//intro to pointers
 	age := 32 //reg var
 
-	//just making things clear
+	//just making things clear - & creates the pointer
 	agePointer := &age
 
 	//we can also declare this in two lines of code
 	//var agePointer *int
 	//agePointer := &age
 
-	fmt.Println("Age", age)
+	//prints out the address - might not be to useful here, maybe when we pass it to a function it will be
+	fmt.Println("Age", agePointer)
+
+	//if we want the actual value we'll need to do something called dereferencing
+	fmt.Println("Age", *agePointer)
 
 	//fmt.Println(getAdultYears())
 	//or create a helper variable
-	adultYears := getAdultYears(age)
-	fmt.Println(adultYears)
+	//adultYears := getAdultYears(age)
+	//fmt.Println(adultYears)
 
 }
 
