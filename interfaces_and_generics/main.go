4285beff48a5de1b84b13c9e7a3e21e015e4183a
lincxx <bossc@BOSSCO>
2023-12-27 16:29:42 -0500

    Embedded Interfaces

--- interfaces_and_generics/main.go
+++ interfaces_and_generics/main.go
@@ -15,6 +15,21 @@ type saver interface {
 	Save() error
 }
 
+// type displayer interface {
+// 	Display()
+// }
+
+// embedded interface
+type outputtable interface {
+	saver
+	Display()
+}
+
+// type outputtable interface {
+// 	Save() error
+// 	Display()
+// }
+
 func main() {
 	title, content := getNoteData()
 	todoText := getUserInput("Todo text")
@@ -33,23 +48,32 @@ func main() {
 		return
 	}
 
-	todo.Display()
-	err = saveData(todo)
+	// todo.Display()
+	// err = saveData(todo)
+	err = outputData(todo)
 
 	if err != nil {
 		fmt.Println("Saving the note failed.")
 		return
 	}
 
-	userNote.Display()
-	err = saveData(userNote)
+	// userNote.Display()
+	// err = saveData(userNote)
+	err = outputData(userNote)
 
-	if err != nil {
-		return
-	}
+	//we could get rid of the err check, since if there was an error the program would stop
+	// if err != nil {
+	// 	return
+	// }
 
 	fmt.Println("Saving the todo succeeded!")
 	fmt.Println("Saving the note succeeded!")
+}
+
+func outputData(data outputtable) error {
+	data.Display()
+	return saveData(data)
+
 }
 
 func saveData(data saver) error {
