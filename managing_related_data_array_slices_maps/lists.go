d6e7c9a6ae88fcf48b59b5b0ece9420c8206d8a5
lincxx <bossc@BOSSCO>
2023-12-28 08:38:34 -0500

    Working with Arrays

--- managing_related_data_array_slices_maps/lists.go
+++ managing_related_data_array_slices_maps/lists.go
@@ -4,7 +4,12 @@ import "fmt"
 
 func main() {
 	//Array
+	//opt 1
+	var productNames [4]string = [4]string{"A book"}
+	//opt 2
 	prices := [4]float64{10.99, 9.99, 45.99, 20.0}
 
+	productNames[2] = "Cool story"
 	fmt.Println(prices[2])
+	fmt.Println(productNames)
 }
