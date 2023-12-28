e01ee94541b214365971f97191ebd3fc83e6062d
lincxx <bossc@BOSSCO>
2023-12-28 10:07:02 -0500

    Diving Deeper Into Slices

--- managing_related_data_array_slices_maps/lists.go
+++ managing_related_data_array_slices_maps/lists.go
@@ -15,7 +15,30 @@ func main() {
 	fmt.Println(prices[2])
 	fmt.Println(productNames)
 
-	// Slices
+	// Slices - are pretty much a window into the array. So it looks or modifies the org array.
 	featuredPrices := prices[1:3]
 	fmt.Println(featuredPrices)
+
+	//omit the starting index
+	featuredPrices2 := prices[:3]
+	fmt.Println(featuredPrices2)
+
+	featuredPrices3 := prices[1:]
+	fmt.Println(featuredPrices3)
+
+	//building slice off of an other slice
+	hightlightedPrices := featuredPrices[:1]
+	fmt.Println(hightlightedPrices)
+
+	//Diving Deeper Into Slices
+	featuredPrices4 := prices[1:]
+	//this overrides the org array
+	featuredPrices4[0] = 199.99
+	hightlightedPrices1 := featuredPrices4[:1]
+	fmt.Println(hightlightedPrices1)
+	fmt.Println(prices)
+	fmt.Println(len(prices))
+	fmt.Println(len(featuredPrices4), cap(featuredPrices4))
+	fmt.Println(len(hightlightedPrices1), cap(hightlightedPrices1))
+
 }
